3594a5edb736d0d1b7edff88516d72ba72eef0d0
javier <gamingjavi21@gmail.com>
2026-05-10 22:01:10 +0700

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,82 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MapView, { Marker, Polyline, UrlTile } from "react-native-maps";
+import axios from "axios";
+import tw from "twrnc";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const [routeCoords, setRouteCoords] = useState([]);
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const pickupCords = {
+    latitude: -7.2761716,
+    longitude: 112.7802211,
+    latitudeDelta: 0.0005,
+    longitudeDelta: 0.0005,
+  };
+
+  const dropLocationCords = {
+    latitude: -7.2686917,
+    longitude: 112.7842194,
+    latitudeDelta: 0.0005,
+    longitudeDelta: 0.005,
+  };
+
+  useEffect(() => {
+    getRoute();
+  }, []);
+
+  const getRoute = async () => {
+    try {
+      const response = await axios.post(
+        "https://api.openrouteservice.org/v2/directions/driving-car/geojson",
+        {
+          coordinates: [
+            [pickupCords.longitude, pickupCords.latitude],
+            [dropLocationCords.longitude, dropLocationCords.latitude],
+          ],
+        },
+        {
+          headers: {
+            Authorization: process.env.EXPO_PUBLIC_ORS_API_KEY,
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      const coords = response.data.features[0].geometry.coordinates.map(
+        (item: any) => ({
+          latitude: item[1],
+          longitude: item[0],
+        }),
+      );
+
+      setRouteCoords(coords);
+    } catch (error: any) {
+      console.log(error.response?.data || error.message);
+    }
+  };
+
+  return (
+    <SafeAreaView style={tw`flex-1`}>
+      <MapView style={tw`flex-1`} initialRegion={pickupCords}>
+        {/* OpenStreetMap */}
+        <UrlTile
+          urlTemplate="https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+          maximumZ={19}
+        />
+        {/* Pickup */}
+        <Marker coordinate={pickupCords} />
+
+        {/* Destination */}
+        <Marker coordinate={dropLocationCords} />
+
+        {/* Route */}
+        <Polyline
+          coordinates={routeCoords}
+          strokeWidth={5}
+          strokeColor="blue"
+        />
+      </MapView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
